d2c5afb84f1d447012ad3197867b74588ba46520
CMunkyDev <matt@jcmueller.com>
2018-02-01 12:32:27 -0800

    refactor for different types of zip generation and posting

--- controllers/templates.js
+++ controllers/templates.js
@@ -3,19 +3,30 @@ const Model = require(`../models/templates.js`)
 
 class TemplatesController extends Controller {
     static postTemplate (req, res, next) {
-        Model.create({owner_id: req.token.id, template_object: req.body})
+        req.body.owner_id = req.token.id
+        Model.create(req.body)
         .then(row => {
             res.json(row)
         })
     }
 
     static sendZip (req, res, next) {
+        Model.generateZip(req.body)
+        .then(zipObject => {
+            res.set('Content-disposition', 'attachment; filename=snapi.zip')
+            res.set('Content-Type', 'application/zip')
+
+            zipObject.generateNodeStream().pipe(res).on('end', res.end)
+        })
+    }
+
+    static sendZipFromDb (req, res, next) {
         Model.getTemplateObject(req.params.id)
         .then(template => {
             return Model.generateZip(template)
         })
         .then(zipObject => {
-            res.set('Content-disposition', 'attachment; filename=test.zip')
+            res.set('Content-disposition', 'attachment; filename=snapi.zip')
             res.set('Content-Type', 'application/zip')
 
             zipObject.generateNodeStream().pipe(res).on('end', res.end)
